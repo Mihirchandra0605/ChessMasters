5d1629c9d02ea2310fab2ad9b298a257dab7e767
K-Niv <160134647+K-Niv@users.noreply.github.com>
2024-09-01 21:01:34 +0530

    The index page

--- Backend/config.js
+++
@@ -1,9 +0,0 @@
-import { configDotenv } from "dotenv";
-
-configDotenv();
-
-export const jwtSecretKey = process.env.JWT_SECRET_KEY;
-
-if (jwtSecretKey == null || jwtSecretKey == "") {
-  throw new Error("JWT_SECRET_KEY environment variable is not set!");
-}
