5accd9a8d9ffc3201a24d288e279f923bf9aa4e3
Mihirchandra Loke <mihirloke05@gmail.com>
2025-05-03 16:51:29 +0530

    shriram

--- config.js
+++ config.js
@@ -1,10 +1,1 @@
-import { config } from "dotenv";
-
-// Load environment variables from .env file
-config();
-
-const {
-MIHIR_BACKEND
-  } = process.env;
-
-export const mihirBackend = MIHIR_BACKEND || "http://localhost:3000"; // default fallback
+export const mihirBackend = import.meta.env.MIHIR_BACKEND || "http://localhost:3000";
